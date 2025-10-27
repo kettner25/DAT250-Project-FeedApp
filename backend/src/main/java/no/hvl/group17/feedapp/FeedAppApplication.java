c0bf98b41a90823deeae07abea3ce77d8bf4116c
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-10-27 15:23:22 +0100

    matej: rename back to correct name

--- backend/src/main/java/no/hvl/group17/feedapp/FeedAppApplication.java
+++ backend/src/main/java/no/hvl/group17/feedapp/FeedAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class tmpFeedAppApplication {
+public class FeedAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(tmpFeedAppApplication.class, args);
+		SpringApplication.run(FeedAppApplication.class, args);
 	}
 
 }
