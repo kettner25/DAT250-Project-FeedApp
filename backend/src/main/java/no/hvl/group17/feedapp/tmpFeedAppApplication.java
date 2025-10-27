54fc6d01a46134f53e77b9356af2bdb51f5c93c5
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-10-27 15:22:46 +0100

    matej: temp filename change to fix case sensitive issues

--- backend/src/main/java/no/hvl/group17/feedapp/tmpFeedAppApplication.java
+++ backend/src/main/java/no/hvl/group17/feedapp/tmpFeedAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FeedAppApplication {
+public class tmpFeedAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FeedAppApplication.class, args);
+		SpringApplication.run(tmpFeedAppApplication.class, args);
 	}
 
 }
--- backend/src/test/java/no/hvl/group17/feedapp/tmpFeedAppApplicationTests.java
+++ backend/src/test/java/no/hvl/group17/feedapp/tmpFeedAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FeedAppApplicationTests {
+class tmpFeedAppApplicationTests {
 
 	@Test
 	void contextLoads() {
