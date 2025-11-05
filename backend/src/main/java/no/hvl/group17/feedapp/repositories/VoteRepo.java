fdea56beb2f4bad65871922a6bcf94f5e9f27731
Lubos Kettner <lubos.kettner@gmail.com>
2025-11-05 19:39:30 +0100

    lubos: Vote repo fix & tests passing

--- backend/src/main/java/no/hvl/group17/feedapp/repositories/VoteRepo.java
+++ backend/src/main/java/no/hvl/group17/feedapp/repositories/VoteRepo.java
@@ -12,9 +12,9 @@ public interface VoteRepo extends JpaRepository<Vote, Integer> {
     @Query("SELECT o.id, COUNT(v) FROM Option o LEFT JOIN o.votes v WHERE o.poll.id = :pid GROUP BY o.id")
     List<Object[]> getVoteCountByPoll(@Param("pid") int id);
 
-    @Query("SELECT true FROM Vote v where v.anonId = :anonID and v.option.id = :oid")
+    @Query("SELECT Count(v) <> 0 FROM Vote v where v.anonId = :anonID and v.option.id = :oid")
     Boolean existsVoteByAnonID(@Param("anonID") String anonID, @Param("oid") int oid);
 
-    @Query("SELECT true FROM Vote v where v.option.id = :oid and v.user.id = :uid")
+    @Query("SELECT Count(v) <> 0 FROM Vote v where v.option.id = :oid and v.user.id = :uid")
     Boolean existsVoteByUserID(@Param("uid") int uid, @Param("oid") int oid);
 }
--- backend/src/test/java/no/hvl/group17/feedapp/repositories/VoteRepoTests.java
+++ backend/src/test/java/no/hvl/group17/feedapp/repositories/VoteRepoTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Instant;
 import java.util.Arrays;
@@ -17,6 +18,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class VoteRepoTests {
     @Autowired
     private UserRepo userRepository;
@@ -30,29 +32,28 @@ public class VoteRepoTests {
     @BeforeEach
     void setup() {
         userRepository.save(User.builder()
-                .id(1)
                 .keycloakId("1")
                 .username("alice")
                 .email("alice@mail.com")
                 .build());
         userRepository.save(User.builder()
-                .id(2)
                 .keycloakId("2")
                 .username("bob")
                 .email("bob@mail.com")
                 .build());
 
-        pollRepository.save(Poll.builder()
-                .id(1)
+        var poll = Poll.builder()
                 .question("Favorite food?")
                 .options(
-                        Arrays.asList(Option.builder().id(1).caption("Pancakes").build(),
-                                Option.builder().id(2).caption("Pizza").build(),
-                                Option.builder().id(3).caption("Dumplings").build())
+                        Arrays.asList(Option.builder().caption("Pancakes").build(),
+                                Option.builder().caption("Pizza").build(),
+                                Option.builder().caption("Dumplings").build())
                 )
                 .user(userRepository.findAll().getFirst())
                 .publishedAt(Instant.now())
-                .build());
+                .build();
+        poll.linkOptions();
+        pollRepository.save(poll);
 
         voteRepository.save(Vote.builder()
                         .option(pollRepository.findAll().getFirst().getOptions().get(1))
@@ -74,47 +75,50 @@ public class VoteRepoTests {
 
     @AfterEach
     void tearDown() {
-        userRepository.deleteAll();
-        pollRepository.deleteAll();
         voteRepository.deleteAll();
+        pollRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
     void getListOfVotesCountByPollID_returnsList() {
-        var list = voteRepository.getVoteCountByPoll(1);
+        var list = voteRepository.getVoteCountByPoll(pollRepository.findAll().getFirst().getId());
 
         assertThat(list.size()).isEqualTo(3);
-        assertThat((int) list.get(1)[0]).isEqualTo(2);
-        assertThat((int) list.get(1)[1]).isEqualTo(3);
+        assertThat((int) list.get(1)[0]).isEqualTo(pollRepository.findAll().getFirst().getOptions().get(1).getId());
+        assertThat((long) list.get(1)[1]).isEqualTo(3);
     }
 
     @Test
     void getListOfVotesCountByPollID_returnsEmpty() {
-        var list = voteRepository.getVoteCountByPoll(2);
+        var list = voteRepository.getVoteCountByPoll(-1);
         assertThat(list).isEmpty();
     }
 
     @Test
     void existsVoteByAnonID_ReturnTrue() {
-        var True = voteRepository.existsVoteByAnonID("10203040506", 2);
+        var True = voteRepository.existsVoteByAnonID("10203040506", pollRepository.findAll().getFirst().getOptions().get(1).getId());
         assertThat(True).isTrue();
     }
 
     @Test
     void existsVoteByAnonID_ReturnFalse() {
-        var False = voteRepository.existsVoteByAnonID("_10203040506", 2);
+        var False = voteRepository.existsVoteByAnonID("_10203040506", pollRepository.findAll().getFirst().getOptions().get(1).getId());
         assertThat(False).isFalse();
     }
 
     @Test
     void existsVoteByUserID_ReturnTrue() {
-        var True = voteRepository.existsVoteByUserID(1, 2);
+        var True = voteRepository.existsVoteByUserID(userRepository.findAll().getFirst().getId(), pollRepository.findAll().getFirst().getOptions().get(1).getId());
         assertThat(True).isTrue();
     }
 
     @Test
     void existsVoteByUserID_ReturnFalse() {
-        var False = voteRepository.existsVoteByUserID(1, 2);
+        var False = voteRepository.existsVoteByUserID(-1, pollRepository.findAll().getFirst().getOptions().get(1).getId());
+        assertThat(False).isFalse();
+
+        False = voteRepository.existsVoteByUserID(userRepository.findAll().getFirst().getId(), pollRepository.findAll().getFirst().getOptions().get(0).getId());
         assertThat(False).isFalse();
     }
 }
