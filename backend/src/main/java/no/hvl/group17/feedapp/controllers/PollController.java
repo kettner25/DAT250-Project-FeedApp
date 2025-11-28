f379798fe67ec5669c38b5046ffb664ac51c11e7
Lubos Kettner <lubos.kettner@gmail.com>
2025-11-28 11:33:23 +0100

    lubos: Returning HTTP status codes

--- backend/src/main/java/no/hvl/group17/feedapp/controllers/PollController.java
+++ backend/src/main/java/no/hvl/group17/feedapp/controllers/PollController.java
@@ -6,6 +6,8 @@ import no.hvl.group17.feedapp.models.OptionCount;
 import no.hvl.group17.feedapp.services.PollService;
 import no.hvl.group17.feedapp.services.UserService;
 import no.hvl.group17.feedapp.services.VoteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -31,51 +33,60 @@ public class PollController {
 
     /// Public
     @GetMapping("/{pid}")
-    public Poll getPoll(@PathVariable int pid) {
-        return pollService.getPollById(pid);
+    public ResponseEntity<Poll> getPoll(@PathVariable int pid) {
+        var poll = pollService.getPollById(pid);
+
+        if (poll == null) return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        return new  ResponseEntity<>(poll, HttpStatus.OK);
     }
 
     /// Authenticated USER or ADMIN
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @PostMapping("")
-    public Poll createPoll(@AuthenticationPrincipal Jwt jwt, @RequestBody Poll poll) {
+    public ResponseEntity<Poll> createPoll(@AuthenticationPrincipal Jwt jwt, @RequestBody Poll poll) {
         if (!poll.Verify()) return null;
 
         int uid = userService.getOrCreateFromJwt(jwt).getId();
         if (hasAdminRole(jwt))
             uid = poll.getUser().getId();
 
-        return pollService.createPoll(uid, poll);
+        var _poll = pollService.createPoll(uid, poll);
+        return new ResponseEntity<>(_poll, _poll != null ? HttpStatus.CREATED :  HttpStatus.BAD_REQUEST);
     }
 
     /// Authenticated USER or ADMIN
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @PutMapping("/{pid}")
-    public Poll editPoll(@AuthenticationPrincipal Jwt jwt, @PathVariable int pid, @RequestBody Poll poll) {
+    public ResponseEntity<Poll> editPoll(@AuthenticationPrincipal Jwt jwt, @PathVariable int pid, @RequestBody Poll poll) {
         if (!poll.Verify()) return null;
 
         int uid = userService.getOrCreateFromJwt(jwt).getId();
         if (hasAdminRole(jwt))
             uid = poll.getUser().getId();
 
-        return pollService.editPoll(uid, pid, poll);
+        var _poll = pollService.editPoll(uid, pid, poll);
+        return new ResponseEntity<>(_poll, _poll != null ? HttpStatus.OK :  HttpStatus.BAD_REQUEST);
     }
 
     /// Authenticated USER or ADMIN
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @DeleteMapping("/{pid}")
-    public Boolean deletePoll(@AuthenticationPrincipal Jwt jwt, @PathVariable int pid) {
+    public ResponseEntity<Boolean> deletePoll(@AuthenticationPrincipal Jwt jwt, @PathVariable int pid) {
         int uid = userService.getOrCreateFromJwt(jwt).getId();
         if (hasAdminRole(jwt))
             uid = pollService.getPollById(pid).getUser().getId();
 
-        return pollService.deletePollById(uid, pid);
+        var bool = pollService.deletePollById(uid, pid);
+        return new ResponseEntity<>(bool, bool ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND);
     }
 
     /// Public
     @GetMapping("/{pid}/count")
-    public List<OptionCount> getVoteCount(@PathVariable int pid) {
-        return voteService.getVoteCountsByPoll(pid);
+    public ResponseEntity<List<OptionCount>> getVoteCount(@PathVariable int pid) {
+        var res = voteService.getVoteCountsByPoll(pid);
+
+        if (res.isEmpty())  return new ResponseEntity<>(res, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
     /// Private helper
